d77b3aa20e773dd5ff66f6a2f5d99b2e1938b002
Arnout Engelen <arnouten@bzzt.net>
2018-10-03 19:40:41 +0200

    Look for MemberUp events

--- bootstrap-demo/kubernetes-api/test.sh
+++ bootstrap-demo/kubernetes-api/test.sh
@@ -12,4 +12,17 @@ kubectl get pods
 
 POD=$(kubectl get pods | grep appka | grep Running | head -n1 | awk '{ print $1 }')
 
-kubectl logs $POD
+TRY=0
+
+until [ $TRY -ge 10 ]
+do
+  echo "Checking for MemberUp logging..."
+  kubectl logs $POD | grep MemberUp
+  [ `kubectl logs $POD | grep MemberUp | wc -l` -eq 2 ] && break
+  sleep 3
+done
+
+if [ $TRY -eq 10 ]
+then
+  exit -1
+fi
